e6ed100e42cb55c37385a0cd12d7b627a6f968ba
rebusnardo <ron.busnardo@gmail.com>
2022-11-26 08:08:37 -0800

    Section Three

---
+++ NZWalks/NZWalks.API/Profiles/RegionsProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionsProfiles: Profile
+    {
+        public RegionsProfiles()
+        {
+            CreateMap<Models.Domain.Region, Models.DTO.Region>().ReverseMap();
+        }
+    }
+}
